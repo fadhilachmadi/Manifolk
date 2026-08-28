f4fdc82186fcafa4f39ba0cdbaee7c87ee573cb8
Muhammad Fadhil Achmadi <fadhil.achmadi@faspay.co.id>
2026-08-28 14:19:46 +0700

    connect posgre

--- database/posgresql.go
+++ database/posgresql.go
@@ -8,6 +8,7 @@ import (
 
 func InitPostgre() (*sql.DB, error) {
 	databaseURL := "postgresql://netlifydb_owner:npg_I0zOC6kXVHdR@ep-frosty-shadow-anc6e5l2.c-6.us-east-1.db.netlify.com/netlifydb?channel_binding=require&sslmode=require"
+	databaseURL = "postgresql://netlifydb_owner:npg_WeUHn48uqyRB@ep-weathered-cherry-anx8qh59.c-6.us-east-1.db.netlify.com/netlifydb?sslmode=require"
 	db, err := sql.Open("pgx", databaseURL)
 	if err != nil {
 		return nil, err
